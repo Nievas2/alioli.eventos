e51f7c2a45900796485ebcacc291e84d59bd67b9
Nievas2 <angelgabrielnievas@gmail.com>
2025-09-25 10:47:34 -0300

    feat(Contact): :sparkles: Add about us

--- src/pages/AboutUs.tsx
+++ src/pages/AboutUs.tsx
@@ -1,6 +1,59 @@
 const AboutUs = () => {
   return (
-    <div>AboutUs</div>
+    <section className="p-8">
+      <div className="flex flex-col justify-center items-center mb-8 w-full">
+        <h2 className="font-bold text-4xl">Sobre Nosotros</h2>
+        <p className="font-extralight">
+          Te contamos quienes somos, porque somos lo que hacemos y como nacio
+          nuestra empresa.
+        </p>
+      </div>
+      <div>
+        <img src="/logo.webp" className="size-64 float-left" alt="Nosotros" />
+      </div>
+
+      <p className="m-4">
+        En el corazón de nuestra historia hay un grupo de amigos unidos por una
+        pasión inquebrantable: el amor por la buena comida, la excelencia en el
+        servicio y la alegría de crear experiencias inolvidables. Fue esa
+        conexión especial la que nos llevó a unirnos con un propósito claro:
+        ofrecer un servicio integral y personalizado para eventos que
+        transformen momentos especiales en recuerdos imborrables.
+      </p>
+
+      <p className="m-4">
+        Nos emociona ser parte de los instantes que marcan la vida, desde
+        celebraciones íntimas hasta grandes eventos. Nos dedicamos a cuidar cada
+        detalle con esmero, desde la planificación hasta la ejecución, para que
+        tú solo tengas que preocuparte por disfrutar. Nuestra misión es simple
+        pero poderosa: queremos que vivas tu evento como un invitado más, libre
+        de preocupaciones y con la certeza de que todo está en las mejores
+        manos.
+      </p>
+
+      <p className="m-4">
+        Para lograrlo, nos esforzamos por estar siempre a la vanguardia. Nos
+        capacitamos de manera constante, explorando nuevas tendencias, técnicas
+        y estilos para ofrecerte un servicio innovador y de calidad. Esta
+        dedicación nos permite presentarte un amplio abanico de opciones, desde
+        menús exquisitos que deleitan el paladar hasta detalles personalizados
+        que reflejan tu visión y estilo. Nos adaptamos a tus necesidades, ya sea
+        que sueñes con una experiencia clásica y elegante o algo moderno y fuera
+        de lo convencional.
+      </p>
+
+      <p className="m-4">
+        Nuestro compromiso va más allá de cumplir expectativas; buscamos
+        superarlas. Cada evento es una oportunidad para demostrar nuestra pasión
+        por lo que hacemos, trabajando con profesionalismo, creatividad y un
+        enfoque humano que nos distingue. Con cada celebración, nuestro objetivo
+        sigue siendo el mismo: que tú y tus seres queridos puedan disfrutar
+        plenamente, confiando en que cada aspecto está cuidadosamente orquestado
+        para hacer de tu día algo verdaderamente especial. Porque en cada
+        evento, nuestro mayor orgullo es verte brillar como el protagonista de
+        tu propia historia.
+      </p>
+    </section>
   )
 }
 export default AboutUs
