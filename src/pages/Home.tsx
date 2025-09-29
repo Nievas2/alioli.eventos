d4785f39f74bfc7dd23c6abeade6125c7a72ab6f
Nievas2 <angelgabrielnievas@gmail.com>
2025-09-29 13:58:32 -0300

    fix(Home): :bug: Fix import

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,6 @@
-import Specialize from "../components/shared/home/events"
+import Specialize from "../components/shared/home/Specialize"
 import ContactForm from "../components/shared/ContactForm"
-import Events from "../components/shared/home/events"
+import Events from "../components/shared/home/Events"
 import Hero from "../components/shared/home/Hero"
 
 const Home = () => {
