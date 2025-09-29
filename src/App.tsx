59d77272da2d6200d1f7467f0d4f23b175921f57
Nievas2 <angelgabrielnievas@gmail.com>
2025-09-29 11:41:43 -0300

    feat(General): :heavy_plus_sign: Add lenis

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,33 @@
 import { Link, Route, Routes } from "react-router-dom"
-import Home from "./pages/Home"
-import AboutUs from "./pages/AboutUs"
-import Contact from "./pages/Contact"
-import Services from "./pages/Services"
 import Navbar from "./components/shared/Navbar"
 import Footer from "./components/shared/Footer"
+import type { LenisRef } from "lenis/react"
+import { useEffect, useRef } from "react"
+import { ReactLenis } from "lenis/react"
+import Services from "./pages/Services"
+import AboutUs from "./pages/AboutUs"
+import Contact from "./pages/Contact"
 import { Icon } from "@iconify/react"
+import Home from "./pages/Home"
 
 function App() {
+  const lenisRef = useRef<LenisRef>(null)
+
+  useEffect(() => {
+    let animationFrameId: number
+    function raf(time: number) {
+      lenisRef.current?.lenis?.raf(time)
+      animationFrameId = requestAnimationFrame(raf)
+    }
+    animationFrameId = requestAnimationFrame(raf)
+
+    return () => {
+      cancelAnimationFrame(animationFrameId)
+    }
+  }, [])
   return (
     <main className="w-full min-h-screen h-full flex flex-col justify-center items-center gap-4 bg-white-main dark:bg-black-main text-black dark:text-white">
-      <div className="max-w-8xl w-full relative">
+      <ReactLenis root ref={lenisRef} className="max-w-8xl w-full relative">
         <Navbar />
 
         <div className="w-full h-full flex flex-col min-h-screen">
@@ -27,7 +44,10 @@ function App() {
           onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
           className="fixed bottom-4 right-4 bg-red-main text-white-main p-2 rounded-full cursor-pointer"
         >
-          <Icon icon="material-symbols:arrow-upward-rounded" className="size-8" />
+          <Icon
+            icon="material-symbols:arrow-upward-rounded"
+            className="size-8"
+          />
         </button>
 
         {/* WhatsApp */}
@@ -41,7 +61,7 @@ function App() {
         </Link>
 
         <Footer />
-      </div>
+      </ReactLenis>
     </main>
   )
 }
