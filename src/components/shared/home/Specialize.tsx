44449e685be923e392164922971ac463320237ed
Nievas2 <angelgabrielnievas@gmail.com>
2025-10-06 12:42:25 -0300

    fix(Home): :bug: Update texts

--- src/components/shared/home/Specialize.tsx
+++ src/components/shared/home/Specialize.tsx
@@ -4,19 +4,18 @@ const Specialize = () => {
       <div className="w-full">
         <div className="bg-opacity-25 bg-blue-secondary dark:backdrop-filter flex flex-col-reverse md:flex-row items-center justify-center gap-10 md:20 py-5 px-5 rounded-xl text-white">
           <div className="flex flex-col gap-5 text-center lg:text-left leading-[normal] max-w-[600px]">
-            <h2 className="text-[24px] lg:text-[36px] font-extrabold lg:text-left">
-              Nos encargamos de que la pases bien
-            </h2>
-
             <p className="text-base lg:text-[20px] leading-[32px]">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis
-              debitis rerum voluptas possimus ut nisi magnam aliquid repellat a
-              fuga et, nobis non id, esse accusamus neque culpa, nesciunt
-              reprehenderit? Lorem ipsum dolor sit amet, consectetur adipisicing
-              elit. Adipisci est, magnam nihil, repudiandae iure aperiam fugit
-              cumque ipsa sequi id totam ipsam rerum, quod suscipit! Repudiandae
-              architecto nisi debitis deleniti.
+              Ofrecemos un servicio completo de catering con menús
+              personalizados, nos encargamos de la comida,mesa dulce, torta,
+              bebida y hasta fin de fiesta.
             </p>
+            <p className="text-base lg:text-[20px] leading-[32px]">
+              Tu única preocupación será disfrutar de tu día especial mientras
+              nosotros nos ocupamos de todo.
+            </p>
+            
+            <p className="text-base lg:text-[20px] leading-[32px]">Desde eventos íntimos hasta grandes celebraciones, nos adaptamos a
+            tus necesidades y presupuesto, no dudes en consultarnos.</p>
           </div>
 
           <img
